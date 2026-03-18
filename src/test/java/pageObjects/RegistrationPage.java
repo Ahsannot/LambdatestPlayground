8c435462b4c10241ae8e263c7880f9755f64ca93
Ahsannot <hafizgee07@gmail.com>
2026-03-18 16:33:53 +0500

    Validate  Registration page open?

--- src/test/java/pageObjects/RegistrationPage.java
+++ src/test/java/pageObjects/RegistrationPage.java
@@ -1,10 +1,34 @@
 package pageObjects;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class RegistrationPage extends BasePage{
 
     public RegistrationPage(WebDriver driver){
         super(driver);
+    }
+
+    //   ************************ LOCATORS ************************
+
+    @FindBy(xpath = "//h1[normalize-space()='Register Account']")
+    WebElement text_RegisterAccount;
+
+    //   ************************ ACTION METHODS ************************
+
+    public String getConfirmationMessage(){
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement confirmation = wait.until(ExpectedConditions.visibilityOf(text_RegisterAccount));
+            return confirmation.getText();
+        } catch (Exception e) {
+            System.out.println("Error fetching confirmation message: " + e.getMessage());
+            return null;
+        }
     }
 }
--- src/test/java/testCases/TC_002DDTRegistrationPageTest.java
+++ src/test/java/testCases/TC_002DDTRegistrationPageTest.java
@@ -1,6 +1,27 @@
 package testCases;
 
+import org.testng.annotations.Test;
+import pageObjects.HomePage;
+import pageObjects.RegistrationPage;
 import testBase.BaseClass;
 
 public class TC_002DDTRegistrationPageTest extends BaseClass {
+
+    @Test
+   public void registrationPageDDTxlx(){
+
+       logger.info("===== Starting Registration Page Test =====");
+
+       HomePage homePage = new HomePage(getDriver());
+       homePage.hoverMyAccount();
+       RegistrationPage registrationPage = homePage.clicklinkRegister();
+
+       validatePageMessageHard(
+               registrationPage.getConfirmationMessage(),
+               "Register Account",
+               "RegistrationPage"
+       );
+       logger.info("===== Registration Page Test Passed =====");
+   }
+
 }
