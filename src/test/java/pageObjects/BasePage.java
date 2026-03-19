6c62436e85263bd04ea57e7928dc823a2722a2c8
Ahsannot <hafizgee07@gmail.com>
2026-03-19 12:32:39 +0500

    modify test data (registration)BasePage and created Registration Page

--- src/test/java/pageObjects/BasePage.java
+++ src/test/java/pageObjects/BasePage.java
@@ -4,6 +4,10 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class BasePage {
 
@@ -12,6 +16,41 @@ public class BasePage {
     public BasePage(WebDriver driver){
         this.driver = driver ;
         PageFactory.initElements(driver, this);
+    }
+
+    public void waitForVisibility(WebElement element) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public void scrollIntoView(WebElement element) {
+        ((org.openqa.selenium.JavascriptExecutor) driver)
+                .executeScript("arguments[0].scrollIntoView({block:'center'});", element);
+    }
+
+    public void waitAndScrollIfNeeded(WebElement element) {
+        try {
+            waitForVisibility(element);
+        } catch (Exception e) {
+            scrollIntoView(element);
+            waitForVisibility(element);
+        }
+    }
+
+    public void safeClick(WebElement element) {
+        try {
+            waitAndScrollIfNeeded(element);
+            element.click();
+        } catch (Exception e) {
+            ((org.openqa.selenium.JavascriptExecutor) driver)
+                    .executeScript("arguments[0].click();", element);
+        }
+    }
+
+    public void safeSendKeys(WebElement element, String value) {
+        waitAndScrollIfNeeded(element);
+        element.clear();
+        element.sendKeys(value);
     }
 
 }
--- src/test/java/pageObjects/RegistrationPage.java
+++ src/test/java/pageObjects/RegistrationPage.java
@@ -3,32 +3,110 @@ package pageObjects;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
+public class RegistrationPage extends BasePage {
 
-public class RegistrationPage extends BasePage{
-
-    public RegistrationPage(WebDriver driver){
+    public RegistrationPage(WebDriver driver) {
         super(driver);
     }
 
-    //   ************************ LOCATORS ************************
-
+    // ************************ LOCATORS ************************
     @FindBy(xpath = "//h1[normalize-space()='Register Account']")
     WebElement text_RegisterAccount;
 
-    //   ************************ ACTION METHODS ************************
+    @FindBy(id = "input-firstname")
+    WebElement input_firstname;
 
-    public String getConfirmationMessage(){
+    @FindBy(id = "input-lastname")
+    WebElement input_lastname;
+
+    @FindBy(id = "input-email")
+    WebElement input_email;
+
+    @FindBy(id = "input-telephone")
+    WebElement input_telephone;
+
+    @FindBy(id = "input-password")
+    WebElement input_password;
+
+    @FindBy(id = "input-confirm")
+    WebElement input_confirm;
+
+    @FindBy(id = "input-newsletter-yes")
+    WebElement radio_subscribe_yes;
+
+    @FindBy(id = "input-newsletter-no")
+    WebElement radio_subscribe_no;
+
+    @FindBy(id = "input-agree")
+    WebElement checkbox_agree;
+
+    @FindBy(xpath = "//input[@value='Continue']")
+    WebElement btn_Continue;
+
+    // ************************ ACTION METHODS ************************
+
+    // Get a confirmation message safely
+    public String getConfirmationMessage() {
         try {
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-            WebElement confirmation = wait.until(ExpectedConditions.visibilityOf(text_RegisterAccount));
-            return confirmation.getText();
+            waitAndScrollIfNeeded(text_RegisterAccount);
+            return text_RegisterAccount.getText();
         } catch (Exception e) {
             System.out.println("Error fetching confirmation message: " + e.getMessage());
             return null;
         }
     }
+
+    // Input fields with safe wait and scroll
+    public void inputFirstName(String firstname) {
+        safeSendKeys(input_firstname, firstname);
+    }
+
+    public void inputLastName(String lastname) {
+        safeSendKeys(input_lastname, lastname);
+    }
+
+    public void inputEmail(String email) {
+        safeSendKeys(input_email, email);
+    }
+
+    public void inputTelephone(String telephone) {
+        safeSendKeys(input_telephone, telephone);
+    }
+
+    public void inputPassword(String password) {
+        safeSendKeys(input_password, password);
+    }
+
+    public void inputConfirm(String confirm) {
+        safeSendKeys(input_confirm, confirm);
+    }
+
+    // Set Subscribe with wait and scroll
+    public void setSubscribe(boolean subscribe) {
+        if (subscribe) {
+            if (!radio_subscribe_yes.isSelected()) {
+                safeClick(radio_subscribe_yes);
+            }
+        } else {
+            if (!radio_subscribe_no.isSelected()) {
+                safeClick(radio_subscribe_no);
+            }
+        }
+    }
+
+    // Set Privacy Policy with wait and scroll
+    public void setPrivacyPolicy(boolean agree) {
+        if (agree && !checkbox_agree.isSelected()) {
+            safeClick(checkbox_agree);
+        } else if (!agree && checkbox_agree.isSelected()) {
+            safeClick(checkbox_agree);
+        }
+    }
+
+    // Submit button with wait and scroll
+    public void submitBtn() {
+        safeClick(btn_Continue);
+    }
+
 }
